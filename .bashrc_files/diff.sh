1701e3949af8ede3c0e07d181b1c8c00d91e6ae3
Rickard Lindberg <ricli85@gmail.com>
2011-08-10 11:30:03 +0200

    Add diffing for svn.

--- .bashrc_files/diff.sh
+++ .bashrc_files/diff.sh
@@ -1,9 +1,15 @@
 vidiff() {
     if _inside_hg_repo; then
         hg diff | vi -
+    elif _inside_svn_repo; then
+        svn diff | vi -
     fi
 }
 
 _inside_hg_repo() {
     hg status > /dev/null 2>&1
+}
+
+_inside_svn_repo() {
+    [ -d .svn ]
 }
