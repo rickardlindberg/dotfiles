50679ee943f50e58d884326aaf64791e2a9421bd
Rickard Lindberg <ricli85@gmail.com>
2011-09-18 22:14:40 +0200

    Remove svn prompt.

--- .bashrc_files/prompt.sh
+++ .bashrc_files/prompt.sh
@@ -2,14 +2,6 @@ _in_color() {
     echo "\e[0;$1m$2\e[m"
 }
 
-_svn_prompt() {
-    if [ -d .svn ]; then
-        if (( $(svn stat | wc -l) != 0 )); then
-            echo "(*)"
-        fi
-    fi
-}
-
 _add_colors_to_ps1() {
     for color in {1..100}; do
         export PS1="$PS1\n[$(_in_color $color "color $color")]"
@@ -21,6 +13,5 @@ export PS1="$PS1\n"
 export PS1="$PS1$(_in_color 34 '\u@\h')"
 export PS1="$PS1$(_in_color 37 ' | ')"
 export PS1="$PS1$(_in_color 33 '\w')"
-export PS1="$PS1$(_in_color 31 ' $(_svn_prompt)')"
 export PS1="$PS1\n"
 export PS1="$PS1\\\$ "
