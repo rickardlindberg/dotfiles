78fe5573dac9c44e620571eb2d78a6a6515ee784
Rickard Lindberg <ricli85@gmail.com>
2011-09-19 09:12:15 +0200

    Add svn to vcs.

--- .bashrc_files/vcs.sh
+++ .bashrc_files/vcs.sh
@@ -1,15 +1,27 @@
 s() {
-    _is_inside_git_repo && git status && return
-    _is_inside_hg_repo  && hg status && return
-    echo "No repository found"
+    if _is_inside_git_repo; then
+        git status "$@"
+    elif _is_inside_hg_repo; then
+        hg status "$@"
+    elif _is_inside_svn_repo; then
+        svn status "$@"
+    elif _is_inside_svn_container_repo; then
+        _do_on_svn_dirs status "$@"
+    else
+        echo "No repository found"
+    fi
 }
 
 d() {
     if _is_inside_git_repo; then
-        git diff
+        git diff "$@" | vi -
     elif _is_inside_hg_repo; then
-        hg diff
-    fi | vi -
+        hg diff "$@" | vi -
+    elif _is_inside_svn_repo; then
+        svn diff "$@" | vi -
+    else
+        echo "No repository found"
+    fi
 }
 
 c() {
@@ -17,6 +29,10 @@ c() {
         git commit "$@"
     elif _is_inside_hg_repo; then
         hg commit "$@"
+    elif _is_inside_svn_repo; then
+        svn commit "$@"
+    else
+        echo "No repository found"
     fi
 }
 
@@ -26,4 +42,23 @@ _is_inside_git_repo() {
 
 _is_inside_hg_repo() {
     hg status > /dev/null 2>&1
+}
+
+_is_inside_svn_repo() {
+    [ -d .svn ]
+}
+
+_is_inside_svn_container_repo() {
+    for dir in *; do
+        [ -d "$dir/.svn" ] && return 0
+    done
+    return 1
+}
+
+_do_on_svn_dirs() {
+    for dir in *; do
+        if [ -d "$dir/.svn" ]; then
+            svn "$@" "$dir"
+        fi
+    done
 }
