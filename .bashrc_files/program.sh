366c57511c725077398f2cf8fea62ba88eeda04e
Rickard Lindberg <ricli85@gmail.com>
2016-04-15 07:49:27 +0200

    History is easier to access with hh.

--- .bashrc_files/program.sh
+++ .bashrc_files/program.sh
@@ -1,6 +1,8 @@
-# Keep history from all terminals
-shopt -s histappend
-PROMPT_COMMAND='history -a'
-
-export HISTSIZE=1000
-export HISTFILESIZE=2500
+export HH_CONFIG=hicolor,keywords         # get more colors
+shopt -s histappend              # append new history items to .bash_history
+export HISTCONTROL=ignorespace   # leading space hides commands from history
+export HISTFILESIZE=10000        # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file syn
+# if this is interactive shell, then bind hh to Ctrl-r (for Vi mode check doc)
+if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hh \C-j"'; fi
