4aa453fa4f9c545f89c96fff6c0ca19a844e1ab6
Rickard Lindberg <ricli85@gmail.com>
2016-11-19 19:13:01 +0100

    Add command to find and open a file.

--- .bashrc_files/aliases.sh
+++ .bashrc_files/aliases.sh
@@ -1,3 +1,33 @@
 alias ll="ls -l"
 alias o=xdg-open
 alias tp=trash-put
+
+f() {
+    local program
+    if [ "$1" = "" ]; then
+        program=vim
+    else
+        program="$1"
+    fi
+    "$program" $( \
+        find . \
+            -type d -name .git -prune \
+            -o \
+            -type d -name .vim -prune \
+            -o \
+            -type d -name .hg -prune \
+            -o \
+            -type d -name __pycache__ -prune \
+            -o \
+            -type d -name venv -prune \
+            -o \
+            -type d -name .cache -prune \
+            -o \
+            -type f -name .*swp -prune \
+            -o \
+            -type f -name *pyc -prune \
+            -o \
+            -type f -print \
+        | rlselect \
+    )
+}
