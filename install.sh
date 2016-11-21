079eec9cf20f72b3fd3b7d142c55785396690619
Rickard Lindberg <ricli85@gmail.com>
2016-11-21 21:17:23 +0100

    Install does not remove inactive dotfiles.

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 install_dotfiles() {
     cd $(dirname "$0")
-    clean_up_old
-    create_directories
-    create_links
+    list_active_dotfiles | clean_up
+    list_inactive_dotfiles | clean_up
+    list_active_dotfiles | create_directories
+    list_active_dotfiles | create_links
 }
 
-clean_up_old() {
-    for dotfile in $(list_dotfiles); do
+clean_up() {
+    while read dotfile; do
         if [ -L ~/$dotfile ]; then
             execute rm ~/$dotfile
         elif [ -e ~/$dotfile ]; then
@@ -18,7 +19,7 @@ clean_up_old() {
 }
 
 create_directories() {
-    for dotfile in $(list_dotfiles); do
+    while read dotfile; do
         dir=$(dirname $dotfile)
         if [ ! -e ~/$dir ]; then
             execute mkdir ~/$dir
@@ -27,12 +28,12 @@ create_directories() {
 }
 
 create_links() {
-    for dotfile in $(list_dotfiles); do
+    while read dotfile; do
         execute ln -s $(pwd)/$dotfile ~/$dotfile
     done
 }
 
-list_dotfiles() {
+list_active_dotfiles() {
     echo .Xmodmap
     echo .bashrc
     echo .bashrc_files
@@ -40,11 +41,8 @@ list_dotfiles() {
     echo .gitignore.global
     echo .hgignore
     echo .hgrc
-    echo .screenrc
     echo .vim
     echo .vimrc
-    echo .xmonad/conkyrc
-    echo .xmonad/xmonad.hs
     echo .config/i3/config
     echo .config/i3/myi3status
     echo bin/sync-folders
@@ -54,6 +52,12 @@ list_dotfiles() {
     echo bin/rlselect
     echo bin/rlselect-history
     echo bin/f
+}
+
+list_inactive_dotfiles() {
+    echo .screenrc
+    echo .xmonad/conkyrc
+    echo .xmonad/xmonad.hs
     echo .i3status
 }
 
