04ab38d54e6b8fe28673d55e28f621168b6645c3
Rickard Lindberg <ricli85@gmail.com>
2016-11-22 06:12:23 +0100

    Replace p(roject) and cdi(nteractive) universal g(o).

--- .bashrc_files/env.sh
+++ .bashrc_files/env.sh
@@ -1,2 +1,14 @@
 export PATH=$HOME/.cabal/bin:$HOME/bin:$PATH
 export EDITOR=vim
+
+g() {
+    local selection
+    if [ "$1" = "" ]; then
+        selection="$(find-dirs ~ 3 | rlselect)"
+    else
+        selection="$(find-dirs "$1" | rlselect)"
+    fi
+    if [ $? = 0 ]; then
+        cd "$selection"
+    fi
+}
